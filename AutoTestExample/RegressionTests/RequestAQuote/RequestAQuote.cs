7931022f48fcb53cf86329461f6de74935f6cb73
secretarev <nikgladkovn@gmail.com>
2022-08-25 10:22:25 +0300

    Add project files.

---
+++ AutoTestExample/RegressionTests/RequestAQuote/RequestAQuote.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AutotestExample.RegressionTests.RequestAQuote
+{
+    [TestFixture]
+    class RequestAQuote
+    {
+        private IWebDriver driver;
+        private string baseURL;
+
+        [SetUp]
+        public void SettingUp()
+        {
+            driver = new ChromeDriver();
+            baseURL = "https://www.dkhardware.com/";
+        }
+
+        [TearDown]
+        public void Quiting()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void OpenRequestAQuotePageFromHeader()
+        {
+            //Go to Main Page
+            driver.Navigate().GoToUrl(baseURL);
+            driver.Manage().Window.Maximize();
+            Assert.AreEqual("Hardware Supply Online | Buy Branded Door Hardware and Parts", driver.Title);
+
+            //Click on Request Qoute link in the header
+            driver.FindElement(By.CssSelector(Selectors.HEADER_SUBMIT_QOUTE_LINK_CSS_SELECTOR)).Click();
+            Assert.AreEqual("Request A Quote",driver.Title);
+        }
+    }
+}
---
+++ AutoTestExample/RegressionTests/Selectors.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutotestExample.RegressionTests
+{
+    class Selectors
+    {
+        //Request A Quote
+        public const String HEADER_SUBMIT_QOUTE_LINK_CSS_SELECTOR = ".top-section-menu > a:nth-child(2)";
+    }
+}
